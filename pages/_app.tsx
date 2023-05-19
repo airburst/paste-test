d8b539d249fb04e72fcb313b59b9b1342ba5892d
Joseph Markus <joseph.markus@simplybusiness.co.uk>
2023-05-19 10:47:47 +0100

    Add some Mobius branding colours

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,24 @@
 import type { AppProps, NextWebVitalsMetric } from "next/app";
-import { Theme } from "@twilio-paste/core/theme";
+// import { Theme } from "@twilio-paste/core/theme";
+import { CustomizationProvider } from "@twilio-paste/core/customization";
 import "../styles/fonts.css";
+import CustomTheme from "./themes/theme.json";
 
 const MyApp: React.FC<React.PropsWithChildren<AppProps>> = ({
   Component,
   pageProps,
 }) => {
   return (
-    <Theme.Provider theme="default">
+    // @ts-expect-error acknowledged bug by Paste creators
+    // https://github.com/twilio-labs/paste/issues/3239#issuecomment-1553613071
+    <CustomizationProvider baseTheme="default" theme={CustomTheme}>
       <style jsx global>{`
         * {
           font-family: "museosans";
         }
       `}</style>
       <Component {...pageProps} />
-    </Theme.Provider>
+    </CustomizationProvider>
   );
 };
 
